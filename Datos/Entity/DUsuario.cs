c50e4eaf9dd913177215561dc801d5f9b653b048
krapto18 <ronny.huerta.18@gmail.com>
2025-07-03 22:18:09 -0500

    CRUD de DUsuario

--- Datos/Entity/DUsuario.cs
+++ Datos/Entity/DUsuario.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.Xml;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Datos.Entity
 {
@@ -20,9 +23,75 @@ namespace Datos.Entity
 			}
 			catch (Exception ex)
 			{
-
 				return ex.Message;
 			}
         }
+		public String Modificar(Usuario usuario) {
+			try
+			{
+				using (var context = new BDFEntities())
+				{
+					Usuario usuarioTemp= context.Usuario.Find(usuario.Codigo);
+					usuarioTemp.Nombre = usuario.Nombre;
+					usuarioTemp.Email= usuario.Email;
+					usuarioTemp.RolId= usuario.RolId;
+					context.SaveChanges();
+                }
+				return "Modificado correctamente";
+			}
+			catch (Exception ex)
+			{
+				
+				return ex.Message;
+			}
+		}
+		public String EliminarFisico(int codigoUsuario) {
+			try
+			{
+				using (var context = new BDFEntities()) {
+					Usuario usuarioTemp = context.Usuario.Find(codigoUsuario);
+					context.Usuario.Remove(usuarioTemp);
+					context.SaveChanges();
+				}
+				return "Usuario eliminado físicamente";
+			}
+			catch (Exception ex)
+			{
+				return ex.Message;	
+			}		
+		}
+        public String EliminarLogico(int codigoUsuario)
+        {
+            try
+            {
+                using (var context = new BDFEntities())
+                {
+                    Usuario usuarioTemp = context.Usuario.Find(codigoUsuario);
+                    usuarioTemp.Estado = 0;
+                    context.SaveChanges();
+                }
+                return "Usuario eliminado lógicamente";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+		public List<Usuario> ListarUsuarios() {
+			List<Usuario> usuarios = new List<Usuario>();
+			try
+			{
+				using (var context = new BDFEntities())
+				{
+					usuarios = context.Usuario.ToList();
+				}
+				return usuarios;
+			}
+			catch (Exception)
+			{
+
+				throw;
+			}
+		}
     }
 }
