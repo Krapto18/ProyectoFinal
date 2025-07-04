e007438b9a65d3508c04ae26f941707e95652daa
krapto18 <ronny.huerta.18@gmail.com>
2025-07-03 21:58:37 -0500

    Agregar archivos de proyecto.

---
+++ Datos/Entity/DUsuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Entity
+{
+    public class DUsuario
+    {
+        public String Registrar(Usuario usuario) {
+			try
+			{
+				using (var context = new BDFEntities())
+				{
+					context.Usuario.Add(usuario);
+					context.SaveChanges();
+				}
+				return "Registrado correctamente";
+			}
+			catch (Exception ex)
+			{
+
+				return ex.Message;
+			}
+        }
+    }
+}
